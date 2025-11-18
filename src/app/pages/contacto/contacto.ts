4a048a03514444a5508dc4dac10d03af80aa8e5d
santiagoandrestorrescastro <santiagoandrestorrescastro@gmail.com>
2025-11-18 05:34:38 +0000

    Se agregan imagenes y se agrega formulario con especificaciones

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,6 +7,46 @@ import { RouterModule } from '@angular/router';
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
-export class Contacto {
+export class Contacto implements AfterViewInit {
 
-}
+  ngAfterViewInit() {
+
+    const telInput: any = document.getElementById('telefono');
+  
+    // Bloquear letras y todo lo que no sea número
+    telInput.addEventListener('keypress', (e: KeyboardEvent) => {
+      if (!/[0-9]/.test(e.key)) {
+        e.preventDefault(); // bloquea letras en tiempo real
+      }
+    });
+  
+    // Bloquear letras incluso si pega texto
+    telInput.addEventListener('input', () => {
+      telInput.value = telInput.value
+        .replace(/[^0-9]/g, '') // solo números
+        .substring(0, 10);      // máximo 10
+    });
+  
+    // Bootstrap Validation
+    const forms = document.querySelectorAll('.needs-validation');
+  
+    Array.from(forms).forEach((form: any) => {
+      form.addEventListener('submit', (event: any) => {
+  
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          event.preventDefault();
+  
+          const alert = document.getElementById('successAlert');
+          alert?.classList.remove('d-none');
+  
+          form.reset();
+          form.classList.remove('was-validated');
+        }
+  
+        form.classList.add('was-validated');
+      });
+    });
+  } }
