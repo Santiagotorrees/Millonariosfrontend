8816c8efe429769ad87d876bc139a9ca66e02b10
santiagoandrestorrescastro <santiagoandrestorrescastro@gmail.com>
2025-11-18 05:52:04 +0000

    Se modifica seccion de historia

--- src/app/pages/historia/historia.ts
+++ src/app/pages/historia/historia.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
   selector: 'app-historia',
   imports: [RouterModule],
   templateUrl: './historia.html',
-  styleUrl: './historia.css',
+  styleUrls: ['./historia.css'],
 })
 export class Historia {
 
